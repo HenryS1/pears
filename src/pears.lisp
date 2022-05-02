9cc4cf53791fd5e597a6d60c33c8a5ec66b3cd23
Henry Steere <henry.steere@gmail.com>
2022-05-02 10:04:59 +0200

    Use indexed stream

--- src/pears.lisp
+++ src/pears.lisp
@@ -36,6 +36,26 @@
 (defstruct stream-end)
 (defparameter *stream-end* (make-stream-end))
 
+(defstruct indexed-stream buffer)
+
+(defun stream-subseq (indexed-stream start end)
+  (subseq (indexed-stream-buffer indexed-stream) start end))
+
+(defun get-entry (indexed-stream i)
+  (let ((buffer (indexed-stream-buffer indexed-stream)))
+    (if (< i (length buffer))
+        (aref buffer i)
+        *stream-end*)))
+
+(defun indexed-file-stream (file-stream)
+  (let ((buffer (make-array (file-length file-stream) :element-type 'character :adjustable nil)))
+    (declare ((simple-array character) buffer))
+    (read-sequence buffer file-stream)
+    (make-indexed-stream :buffer buffer)))
+
+(defun indexed-string-stream (str)
+  (make-indexed-stream :buffer str))
+
 (defun buffer-stream (strm)
   (declare (optimize (speed 3)))
   (let ((buffered (make-bs :strm strm)))
@@ -66,21 +86,23 @@
                  (t (lazy-cons next (rec next-stream)))))))
     (rec stream)))
 
-(defun take-while (p lazy-stream)
-  (labels ((rec (acc s)
-             (if (null s)
-                 (values (reverse acc) s)
-                 (if (funcall p (car s))
-                     (rec (cons (car s) acc) (tail s))
-                     (values (reverse acc) s)))))
-    (rec nil lazy-stream)))
+(defun take-while (p stream i)
+  (labels ((rec (end)
+             (let ((entry (get-entry stream end)))
+               (etypecase entry
+                 (stream-end end)
+                 (t (if (funcall p entry)
+                        (rec (+ end 1))
+                        end))))))
+    (rec i)))
 
-(defun drop-while (p lazy-stream)
-  (if (null lazy-stream) 
-      lazy-stream
-      (if (funcall p (car lazy-stream))
-          (drop-while p (tail lazy-stream))
-          lazy-stream)))
+(defun drop-while (p lazy-stream i)
+  (let ((entry (get-entry lazy-stream i)))
+    (etypecase entry
+      (stream-end i)
+      (t (if (funcall p entry)
+             (drop-while p lazy-stream (+ i 1))
+             i)))))
 
 (defstruct parser f)
 
@@ -91,71 +113,72 @@
 (defun new-parser (f)
   (make-parser :f f))
 
-(defun apply-parser (p strm)
-  (funcall (parser-f p) strm))
+(defun apply-parser (p strm i)
+  (funcall (parser-f p) strm i))
 
-(defun transform-sep-stream (parser sep-parser stream)
-  (and stream
-       (multiple-value-bind (result next-stream) (apply-parser parser stream)
-         (if (eq result *failure*)
-             (multiple-value-bind (sep-result sep-stream) (apply-parser sep-parser stream)
-               (if (eq sep-result *failure*)
-                   nil
-                   (transform-sep-stream parser sep-parser sep-stream)))
-             (lazy-cons result (transform-sep-stream parser sep-parser next-stream))))))
+;; (defun transform-sep-stream (parser sep-parser stream)
+;;   (labels ((rec (i)
+;;              (and stream
+;;                   (multiple-value-bind (result next-i) (apply-parser parser stream 0)
+;;                     (if (eq result *failure*)
+;;                         (multiple-value-bind (sep-result sep-i) (apply-parser sep-parser stream next-i)
+;;                           (if (eq sep-result *failure*)
+;;                               nil
+;;                               (transform-sep-stream parser sep-parser stream sep-i)))
+;;                         (lazy-cons result (transform-sep-stream parser sep-parser next-i))))))) ))
 
-(defun transform-stream (parser stream)
-  (and stream 
-       (multiple-value-bind (result next-stream) (apply-parser parser stream)
-         (if (eq result *failure*)
-             *failure*
-             (transform-stream parser next-stream)))))
+;; (defun transform-stream (parser stream)
+;;   (and stream 
+;;        (multiple-value-bind (result next-stream) (apply-parser parser stream)
+;;          (if (eq result *failure*)
+;;              *failure*
+;;              (transform-stream parser next-stream)))))
 
-(defun fold-stream (f stream val)
-  (if (null stream)
-      val
-      (fold-stream f (tail stream) (funcall f val (head stream)))))
+;; (defun fold-stream (f stream val)
+;;   (if (null stream)
+;;       val
+;;       (fold-stream f (tail stream) (funcall f val ( stream)))))
 
 (defmethod fmap (f (p parser))
-  (new-parser (lambda (strm) 
-                (multiple-value-bind (result new-stream) (apply-parser p strm)
+  (new-parser (lambda (strm i) 
+                (multiple-value-bind (result new-i) (apply-parser p strm i)
                   (if (eq result *failure*) 
                       *failure* 
-                      (values (funcall f result) new-stream))))))
+                      (values (funcall f result) new-i))))))
 
 (defmethod flatmap (f (p parser))
-  (new-parser (lambda (strm)
-     (multiple-value-bind (result new-stream) (apply-parser p strm)
+  (new-parser (lambda (strm i)
+     (multiple-value-bind (result new-i) (apply-parser p strm i)
        (if (eq result *failure*)
            *failure*
-           (apply-parser (funcall f result) new-stream))))))
+           (apply-parser (funcall f result) strm new-i))))))
 
 (defmacro sequential (&rest body)
-  (labels ((nest (parser-bindings cur-stream value-form)
-             (if (null parser-bindings)
-                 `(values ,value-form ,cur-stream)
-                 (let* ((bindings (car parser-bindings))
-                        (result-binding (car bindings))
-                        (cur-parser (cadr bindings))
-                        (next-result (gensym))
-                        (next-stream (gensym)))
-                   `(multiple-value-bind (,next-result ,next-stream)
-                        (apply-parser ,cur-parser ,cur-stream)
-                      (if (eq ,next-result *failure*)
-                          *failure*
-                          ,(if (string= (symbol-name result-binding) "_")
-                               (nest (cdr parser-bindings) next-stream value-form)
-                              `(let ((,result-binding ,next-result))
-                                 ,(nest (cdr parser-bindings) next-stream value-form)))))))))
-    (let* ((init-stream (gensym))
-           (parser-bindings (butlast body))
-           (evaluated-parsers (loop for (b p) in parser-bindings collect (list (gensym) nil)))
-           (bindings (loop for (b p) in parser-bindings for (sym nl) in evaluated-parsers
+  (let ((stream (gensym)) (init-i (gensym)))
+    (labels ((nest (parser-bindings cur-i value-form)
+               (if (null parser-bindings)
+                   `(values ,value-form ,cur-i)
+                   (let* ((bindings (car parser-bindings))
+                          (result-binding (car bindings))
+                          (cur-parser (cadr bindings))
+                          (next-result (gensym))
+                          (next-i (gensym)))
+                     `(multiple-value-bind (,next-result ,next-i)
+                          (apply-parser ,cur-parser ,stream ,cur-i)
+                        (if (eq ,next-result *failure*)
+                            *failure*
+                            ,(if (string= (symbol-name result-binding) "_")
+                                 (nest (cdr parser-bindings) next-i value-form)
+                                 `(let ((,result-binding ,next-result))
+                                    ,(nest (cdr parser-bindings) next-i value-form)))))))))
+      (let* ((parser-bindings (butlast body))
+             (evaluated-parsers (loop for (b p) in parser-bindings collect (list (gensym) nil)))
+             (bindings (loop for (b p) in parser-bindings for (sym nl) in evaluated-parsers
                           collect `(,b (or ,sym (setf ,sym ,p)))))
-           (value-form (car (last body))))
-      `(new-parser (let ,evaluated-parsers 
-                     (lambda (,init-stream)
-                       ,(nest bindings init-stream value-form)))))))
+             (value-form (car (last body))))
+        `(new-parser (let ,evaluated-parsers 
+                       (lambda (,stream ,init-i)
+                         ,(nest bindings init-i value-form))))))))
 
 (defmacro orp (&rest parsers)
   (if (null parsers)
@@ -165,55 +188,61 @@
              (parsers (mapcar (lambda (sym p) `(or ,(car sym) (setf ,(car sym) ,p))) 
                               evaluated-parsers reversed-parsers))
              (stream (gensym))
+             (init-i (gensym))
              (parse-attempts (reduce (lambda (acc p)
                                       (let ((result (gensym))
-                                            (next-stream (gensym)))
-                                        `(multiple-value-bind (,result ,next-stream) 
-                                             (apply-parser ,p ,stream)
+                                            (next-i (gensym)))
+                                        `(multiple-value-bind (,result ,next-i) 
+                                             (apply-parser ,p ,stream ,init-i)
                                            (if (eq ,result *failure*)
                                                ,acc
-                                               (values ,result ,next-stream)))))
+                                               (values ,result ,next-i)))))
                                      (cdr parsers)
                                      :initial-value 
-                                     `(apply-parser ,(car parsers) ,stream))))
+                                     `(apply-parser ,(car parsers) ,stream ,init-i))))
         `(new-parser (let ,evaluated-parsers 
-                       (lambda (,stream) ,parse-attempts))))))
+                       (lambda (,stream ,init-i) ,parse-attempts))))))
 
 (defun one (pred) 
-  (new-parser (lambda (stream)
-                (if (null stream)
-                    *failure*
-                    (let ((c (head stream)))
-                      (if (funcall pred c)
-                          (values (head stream) (tail stream))
-                          *failure*))))))
+  (new-parser (lambda (stream i)
+                (let ((entry (get-entry stream i)))
+                  (etypecase entry
+                    (stream-end *failure*)
+                    (t (if (funcall pred entry)
+                           (values entry (+ i 1))
+                           *failure*)))))))
 
 (defun many (pred)
-  (new-parser (lambda (stream) 
-                (take-while pred stream))))
+  (new-parser (lambda (stream i) 
+                (let ((end (take-while pred stream i)))
+                  (values (stream-subseq stream i end) end)))))
 
 (defun many1 (pred)
-  (sequential (fst (one pred))
-              (rest (many pred))
-              (cons fst rest)))
+  (new-parser (lambda (stream i)
+                (let ((end (take-while pred stream i)))
+                  (if (= i end)
+                      *failure*
+                      (values (stream-subseq stream i end) end))))))
 
 (defun repeated (parser)
-  (new-parser (lambda (stream)
-                (labels ((rec (acc new-stream)
-                           (multiple-value-bind (result next-stream) 
-                               (apply-parser parser new-stream)
-                             (if (eq result *failure*)
-                                 (values (reverse acc) new-stream)
-                                 (rec (cons result acc) next-stream)))))
-                  (rec nil stream)))))
+  (new-parser (lambda (stream i)
+                (loop for end-prev = i then end
+                   for (result end) = (multiple-value-list (apply-parser parser stream i))
+                   then (multiple-value-list (apply-parser parser stream end))
+                   while (not (eq result *failure*))
+                   collect result into results
+                   finally (return (values results end-prev))))))
 
 (defun repeated1 (parser)
-  (new-parser (lambda (stream)
-                (multiple-value-bind (result next-stream) 
-                    (apply-parser (repeated parser) stream)
-                  (if (or (eq result *failure*) (null result))
-                      *failure*
-                      (values result next-stream))))))
+  (new-parser (lambda (stream i)
+                (loop for end-prev = i then end
+                   for (result end) = (multiple-value-list (apply-parser parser stream i))
+                   then (multiple-value-list (apply-parser parser stream end))
+                   while (not (eq result *failure*))
+                   collect result into results
+                   finally (return (if (null results)
+                                       *failure*
+                                       (values results end-prev)))))))
 
 (defun sep-by (value-parser sep-parser)
   (let ((sep-parser (sequential (_ sep-parser)
@@ -224,8 +253,8 @@
                 (cons fst rest))))
 
 (defun discard (pred)
-  (new-parser (lambda (stream)
-                (values nil (drop-while pred stream)))))
+  (new-parser (lambda (stream i)
+                (values nil (drop-while pred stream i)))))
 
 (defun whitespacep (c)
   (or (char= c #\space) (char= c #\newline)
@@ -239,37 +268,37 @@
 (defun newlinep (c) (or (char= c #\newline) (char= c #\return)))
 
 (defun seq (s &key (test #'equal))
-  (labels ((rec (i cur-stream)
-             (if (= i (length s))
-                 (values s cur-stream)
-                 (if (null cur-stream)
-                     *failure*
-                     (if (funcall test (aref s i) (head cur-stream))
-                         (rec (+ i 1) (tail cur-stream))
-                         *failure*)))))
-    (new-parser (lambda (stream) (rec 0 stream)))))
+  (labels ((rec (stream cnt i)
+             (if (= cnt (length s))
+                 (values s i)
+                 (let ((entry (get-entry stream i)))
+                   (etypecase entry
+                     (stream-end *failure*)
+                     (t (if (funcall test (aref s cnt) entry)
+                          (rec stream (+ cnt 1) (+ i 1))
+                          *failure*)))))))
+    (new-parser (lambda (stream i) (rec stream 0 i)))))
 
 (defun optional (parser)
-  (new-parser (lambda (stream)
-                (multiple-value-bind (result next-stream) (apply-parser parser stream)
+  (new-parser (lambda (stream i)
+                (multiple-value-bind (result next-i) (apply-parser parser stream i)
                   (if (eq result *failure*)
-                      (values nil stream)
-                      (values (list result) next-stream))))))
+                      (values nil i)
+                      (values (list result) next-i))))))
 
-(defun repeat-n (predicate n)
-  (new-parser (lambda (stream)
-                (labels ((rec (acc i cur-stream)
-                           (if (= i 0)
-                               (values (reverse acc) cur-stream)
-                               (if (null cur-stream)
-                                   *failure*
-                                   (if (funcall predicate (head cur-stream))
-                                       (rec (cons (head cur-stream) acc) (- i 1) (tail cur-stream))
-                                       *failure*)))))
-                  (rec nil n stream)))))
+(defun manyn (predicate n)
+  (new-parser (lambda (stream i)
+                (loop for cur-i = i then (+ cur-i 1)
+                   for cnt = 0 then (+ cnt 1)
+                   for entry = (get-entry stream cur-i)
+                   while (and (funcall predicate entry) (< cnt n))
+                   collect entry into results
+                   finally (return (if (= cnt n)
+                                       (values results i)
+                                       *failure*))))))
 
 (defun digits-to-int (digits)
-  (loop for d in digits
+  (loop for d across digits
      for n = (digit-char-p d) then (+ (* n 10) (digit-char-p d))
      finally (return n)))
 
@@ -278,7 +307,9 @@
 
 (defparameter *positive-int* (sequential (fst (non-zero-digit))
                                          (rest (many #'digit-char-p))
-                                         (digits-to-int (cons fst rest))))
+                                         (+ (* (expt 10 (length rest)) 
+                                               (digit-char-p fst))
+                                            (digits-to-int rest))))
 
 (defparameter *non-negative-int* (orp (sequential (_ (char1 #\0)) 0) *positive-int*))
 
@@ -313,7 +344,7 @@
 (defun unicode-char ()
   (sequential (_ (char1 #\\))
               (_ (char1 #\u))
-              (cs (repeat-n #'hexp 4))
+              (cs (manyn #'hexp 4))
               (cons #\\ (cons #\u cs))))
 
 (defun escaped-character ()
